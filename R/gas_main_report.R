bf384aac8ed5296dffec2c011fecc14a8c228cda
selesnow <selesnow@gmail.com>
2025-03-19 11:15:18 +0200

    Добавил часть кода для генерации отчёта по лицензиями в разрезе компаний и технических юзеров

--- R/gas_main_report.R
+++ R/gas_main_report.R
@@ -1,5 +1,9 @@
 library(purrr)
+library(stringr)
+library(dplyr)
+library(tidyr)
 
+# Список продуктов, по которым будем запрашивать лицензии
 products <- c(
   'Google-Apps',
   '101031',
@@ -14,18 +18,49 @@ products <- c(
 )
 
 
-
 # Основная функция
 main <- function() {
 
   # Получаем данные о пользователях
-  users_data <- gas_get_users_data()
+  users_data <- gas_get_users_data() %>%
+                rowwise() %>%
+                mutate(
+                  org_unit = orgUnitPath %>% str_remove('^/') %>% str_split('/') %>% pluck(1, 1),
+                  isTechMails = str_detect(orgUnitPath, 'Tech Mails')
+                ) %>%
+                ungroup()
 
   # Получаем данные об использовании диска
   drive_usage <- gas_get_user_drive_usage(emails = 'all', date = Sys.Date() - 4)
 
   # Получаем список лицензий
   license <- map_dfr(.x = products, gas_get_user_licenses) %>%
+             filter(skuName != 'Cloud Identity Free')
+
+  # Делаем отчёт по лицензиям
+  # Отчёт будет загружаться на отдельный лист
+  license_report <- license %>%
+                    right_join(users_data, join_by(userId == primaryEmail)) %>%
+                    select(
+                      'userId',
+                      'skuName',
+                      'org_unit',
+                      'isTechMails'
+                    ) %>%
+                    mutate(
+                      skuName = if_else(isTechMails, str_glue('{skuName} [Tech acc]'), str_glue('{skuName} [Users]'))
+                    ) %>%
+                    arrange(skuName) %>%
+                    summarise(
+                      licens_num = n_distinct(userId),
+                      .by = c('org_unit', 'skuName')
+                    ) %>%
+                    pivot_wider(id_cols = org_unit, names_from = skuName, values_from = licens_num, values_fill = 0) %>%
+                    rename('Org. Unit' = org_unit)
+
+  # Приводим данные о лицензиях к структуре,
+  # необходимой для связки с общими данными по пользователям
+  license <- license %>%
              summarise(
                skuId = paste(skuId, collapse = ', '),
                skuName = paste(skuName, collapse = ', '),
@@ -37,7 +72,7 @@ main <- function() {
   final_data <- users_data %>%
     unnest_wider(name) %>%
     mutate(
-      nickname = NA,#ifelse(!is.null(aliases), aliases[1], NA),
+      nickname = NA,
       first_name = givenName,
       last_name = familyName,
       primary_email = primaryEmail,
